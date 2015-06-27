e2cde0416b8060d89b477b145e40510b93364fc6
victor73 <victor73@github.com>
2015-06-27 16:10:22 -0400

    Merged setup_py branch for packaging and namespace.

--- setup.py
+++ setup.py
@@ -6,24 +6,24 @@ def read(fname):
       return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='cutlass',
-      description='A iHMP domain specific API using osdf-python',
-      long_description=read('README.md'),
-      version='0.0.1',
-      author='Victor Felix',
-      author_email='victor73@gmail.com',
-      url='http://ihmpdcc.org',
-      license='MIT',
-      packages=['cutlass'],
-      requires=['httplib', 'osdf'],
-      classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Intended Audience :: Science/Research",
-        "Natural Language :: English",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Utilities",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Bio-Informatics"
-      ]
-     )
+  description='A iHMP domain specific API using osdf-python',
+  long_description=read('README.md'),
+  version='0.0.1',
+  author='Victor F',
+  author_email='victor73@github.com',
+  url='http://ihmpdcc.org',
+  license='MIT',
+  packages=['cutlass'],
+  requires=['osdf'],
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Intended Audience :: Science/Research",
+    "Natural Language :: English",
+    "Operating System :: POSIX",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Utilities",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+  ]
+)
