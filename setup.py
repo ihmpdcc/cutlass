8cc1ba897cd4d0b4e61f85921b0c892a04458dad
victor73 <victor73@github.com>
2015-06-27 16:09:57 -0400

    Merged setup_py branch for packaging and namespace.

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from distutils.core import setup
+
+# Utility function to read files. Used for the long_description.
+def read(fname):
+      return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='cutlass',
+      description='A iHMP domain specific API using osdf-python',
+      long_description=read('README.md'),
+      version='0.0.1',
+      author='Victor Felix',
+      author_email='victor73@gmail.com',
+      url='http://ihmpdcc.org',
+      license='MIT',
+      packages=['cutlass'],
+      requires=['httplib', 'osdf'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+      ]
+     )
