5f185315e828e097be277cecbf65308ffe299c29
victor73 <victor73@github.com>
2015-06-29 23:16:27 -0400

    Ease of import.

---
+++ cutlass/__init__.py
@@ -0,0 +1,13 @@
+from .iHMPSession import iHMPSession
+from .Project import Project
+from .Study import Study
+from .Visit import Visit
+from .Subject import Subject
+from .SixteenSDnaPrep import SixteenSDnaPrep
+from .SixteenSRawSeqSet import SixteenSRawSeqSet
+from .aspera import aspera
+from .mixs import MIXS
+from .mims import MIMS
+from .mimarks import MIMARKS
+
+#__all__ = ["iHMPSession", "Project", "Study", "Visit", "Subject", "Sample", "SixteenSDnaPrep", "SixteenSRawSeqSet", "MIXS", "MIMS", "MIMARKS"]
