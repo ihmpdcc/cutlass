1e4001d5888475534cae69c351ceb8c8709e972a
schwager-hsph <schwager@hsph.harvard.edu>
2015-12-09 16:55:21 -0500

    some nodes don't have a subtype

--- cutlass/Study.py
+++ cutlass/Study.py
@@ -364,7 +364,7 @@ class Study(Base):
         study._center = study_data['meta']['center']
         study._contact = study_data['meta']['contact']
         study._tags = study_data['meta']['tags']
-        study._subtype = study_data['meta']['subtype']
+        study._subtype = study_data['meta'].get('subtype', None)
 
         if 'srp_id' in study_data['meta']:
             study._srp_id = study_data['meta']['srp_id']
