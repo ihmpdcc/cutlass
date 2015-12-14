7c268fcf1db53dc6e7e7bdf75233deda0073510c
victor73 <victor73@github.com>
2015-12-13 23:26:14 -0500

    Converted to bash.

--- examples/test-upload.sh
+++ examples/test-upload.sh
@@ -1,16 +1,15 @@
-#!/bin/tcsh
+#!/bin/bash
 
 # path to ascp Aspera command-line client (Linux)
-#setenv PATH /home/jcrabtree/.aspera/connect/bin:$PATH
+# export PATH=$HOME/.aspera/connect/bin:$PATH
 
 # path to ascp Aspera command-line client (Mac OS X)
-setenv PATH ~/Applications/Aspera\ Connect.app/Contents/Resources:$PATH
+export PATH=$HOME/Applications/Aspera\ Connect.app/Contents/Resources:$PATH
 
-setenv PYTHONPATH cutlass:osdf-python
+export PYTHONPATH=cutlass:osdf-python
 
 # iHMPDCC production server
-setenv SERVER osdf.ihmpdcc.org
+export SERVER=osdf.ihmpdcc.org
 
 # upload new nodes
 ./test-upload.py --username=$USER --password=$PASS --server=$SERVER --tag=jctest
-
