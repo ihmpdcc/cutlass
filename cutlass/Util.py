760da4f57cea670cd95c2219ed00cecaacb669e9
Victor <victor73@gmail.com>
2016-04-20 16:46:01 -0400

    New decorator for checking that dates are in the past.

--- cutlass/Util.py
+++ cutlass/Util.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+DATE_FORMAT = '%Y-%m-%d'
 
 def enforce_dict(func):
     def wrapper(self, arg):
@@ -20,5 +23,20 @@ def enforce_string(func):
         if type(arg) is not str:
             raise ValueError("Invalid type provided. Must be a string.")
         func(self, arg)
+
+    return wrapper
+
+def enforce_past_date(func):
+    def wrapper(self, date):
+        try:
+            parsed = datetime.strptime(date, DATE_FORMAT)
+        except ValueError:
+            raise ValueError("Invalid date. Must be in YYYY-MM-DD format.")
+
+        now = datetime.now()
+        if parsed > now:
+            raise ValueError("Date must be in the past, not the future.")
+
+        func(self, date)
 
     return wrapper
