426b852833c4c6020e424c691e96217500ce845a
victor73 <victor73>
2016-05-11 12:02:05 -0400

    Support for missing local file upload.

--- examples/abundance_matrix.py
+++ examples/abundance_matrix.py
@@ -13,6 +13,10 @@ password = "test"
 
 session = iHMPSession(username, password)
 
+print("Creating a temp file for example/testing purposes.")
+temp_file = tempfile.NamedTemporaryFile(delete=False).name
+print("Local file: %s" % temp_file)
+
 print("Required fields: ")
 print(AbundanceMatrix.required_fields())
 
@@ -22,10 +26,10 @@ matrix.comment = "test comment"
 matrix.checksums = { "md5": "72bdc024d83226ccc90fbd2177e78d56" }
 matrix.format = "fastq"
 matrix.format_doc = "http://format.url"
-matrix.matrix_type = "matrix_type"
+matrix.matrix_type = "host_cytokine"
 matrix.study = "prediabetes"
 matrix.size = 1000
-matrix.urls = [ "a", "b", "c" ]
+matrix.local_file = temp_file
 
 # Optional properties
 matrix.sop = "the SOP"
@@ -33,7 +37,7 @@ matrix.sop = "the SOP"
 # Annotations are 'computed_from' a 16S trimmed sequence set
 matrix.links = { "computed_from": [ "9bb18fe313e7fe94bf243da07e0032e4" ] }
 
-matrix.tags = [ "abundance", "ihmp" ]
+matrix.tags = [ "test", "abundance", "ihmp" ]
 matrix.add_tag("matrix")
 
 print(matrix.to_json(indent=2))
