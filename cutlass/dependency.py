fd583fb908686f2a0f49b232047d6cda25020216
Victor <victor73@gmail.com>
2016-06-07 17:15:43 -0400

    children() support for HostAssayPrep and MicrobiomeAssayPrep.

--- cutlass/dependency.py
+++ cutlass/dependency.py
@@ -8,21 +8,23 @@ from .Sample import Sample
 from .WgsDnaPrep import WgsDnaPrep
 from .SixteenSDnaPrep import SixteenSDnaPrep
 from .SixteenSRawSeqSet import SixteenSRawSeqSet
+from .MicrobiomeAssayPrep import MicrobiomeAssayPrep
 from .HostAssayPrep import HostAssayPrep
 
 # currently used in Base.children()
 # __name__ attribute used to ensure that if the class or method name
 # changes, the maintainer is forced to update it here, too.
 dependency_methods = {
-              Project.__name__ : Project.studies.__name__,
-                Study.__name__ : Study.subjects.__name__,
-              Subject.__name__ : Subject.visits.__name__,
-                Visit.__name__ : Visit.samples.__name__,
-               Sample.__name__ : Sample.dnaPreps.__name__,
-           WgsDnaPrep.__name__ : WgsDnaPrep.raw_seq_sets.__name__,
-      SixteenSDnaPrep.__name__ : SixteenSDnaPrep.raw_seq_sets.__name__,
-    SixteenSRawSeqSet.__name__ : SixteenSRawSeqSet.trimmed_seq_sets.__name__,
-        HostAssayPrep.__name__ : HostAssayPrep.cytokines.__name__
+                  Project.__name__  : Project.studies.__name__,
+                    Study.__name__  : Study.subjects.__name__,
+                  Subject.__name__  : Subject.visits.__name__,
+                    Visit.__name__  : Visit.samples.__name__,
+                   Sample.__name__  : Sample.dnaPreps.__name__,
+               WgsDnaPrep.__name__  : WgsDnaPrep.raw_seq_sets.__name__,
+          SixteenSDnaPrep.__name__  : SixteenSDnaPrep.raw_seq_sets.__name__,
+        SixteenSRawSeqSet.__name__  : SixteenSRawSeqSet.trimmed_seq_sets.__name__,
+            HostAssayPrep.__name__  : HostAssayPrep.cytokines.__name__,
+      MicrobiomeAssayPrep.__name__  : MicrobiomeAssayPrep.cytokines.__name__
 }
 
 def generator_flatten(gen):
