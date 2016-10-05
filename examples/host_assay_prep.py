80dd841494030652c698e5f91c1901b6945bdd32
Victor <victor73@gmail.com>
2016-10-05 16:15:48 -0400

    Adjusted required/optional properties.

--- examples/host_assay_prep.py
+++ examples/host_assay_prep.py
@@ -24,11 +24,8 @@ prep.center = "the center"
 prep.contact = "first name last name"
 prep.sample_name = "name of the sample"
 prep.experiment_type = "PRIDE:0000429, Shotgun proteomics"
-prep.prep_id = "the prep id"
-prep.storage_duration = 30
 prep.study = "prediabetes"
 prep.title = "the title"
-
 
 # Optional properties
 prep.short_label = "the short label"
@@ -37,10 +34,12 @@ prep.species = "the species"
 prep.cell_type = "the cell type"
 prep.tissue = "test tissue"
 prep.reference = "the reference"
+prep.prep_id = "the prep id"
 prep.protocol_name = "name of the protocol"
 prep.protocol_steps = "steps of the protocol"
 prep.exp_description = "exp description"
 prep.sample_description = "description of the sample"
+prep.storage_duration = 30
 
 print("Creating a temp file for example/testing purposes.")
 temp_file = tempfile.NamedTemporaryFile(delete=False).name
